13ab21cd8eac29cd24cf598af3b2443d22a6f531
JuanJose <jjrodrigur@eafit.edu.co>
2025-05-06 00:03:08 -0500

    Agregar prenda y vista de administrador

--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Prenda
+from .forms import PrendaForm
 # Create your views here.
 
 def lista_prendas(request):
@@ -12,3 +13,18 @@ def home(request):
 
 def contacto(request):
     return render(request, 'contacto.html')
+
+def admin_dashboard(request):
+    return render(request, 'admin_dashboard.html')
+
+def agregar_prenda(request):
+    if request.method == 'POST':
+        form = PrendaForm(request.POST, request.FILES)
+        if form.is_valid():
+            prenda = form.save(commit=False)
+            prenda.disponible = True  # siempre disponible al registrarla
+            prenda.save()
+            return redirect('admin_dashboard')
+    else:
+        form = PrendaForm()
+    return render(request, 'agregar_prenda.html', {'form': form})
