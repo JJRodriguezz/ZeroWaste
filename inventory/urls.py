5e5d53f89f02b5d79ec859c97956bfa6586402f3
JuanJose <jjrodrigur@eafit.edu.co>
2025-05-06 00:02:37 -0500

    Añadidas URLs para dashboard de administrador y formulario de agregar prenda

--- inventory/urls.py
+++ inventory/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),  # Home
     path('inventario/', views.lista_prendas, name='lista_prendas'), 
-    path('contacto/', views.contacto, name='contacto')
+    path('contacto/', views.contacto, name='contacto'),
+    path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
+    path('agregar-prenda/', views.agregar_prenda, name='agregar_prenda'),
 ]
