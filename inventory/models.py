58b5b360ac3ef3f65aeacce7852b9e06d1f5ea10
JuanJose <jjrodrigur@eafit.edu.co>
2025-05-11 15:02:54 -0500

    Agregados modelos: 'Grupo','Categoría','Subcategoría'

--- inventory/models.py
+++ inventory/models.py
@@ -2,22 +2,41 @@ from django.db import models
 
 # Create your models here.
 
+class Grupo(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=50)
+    grupo = models.ForeignKey(Grupo, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.nombre} ({self.grupo})"
+    
+class Subcategoria(models.Model):
+    nombre = models.CharField(max_length=50)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def __str__(self):
+        if self.nombre.lower() == self.categoria.nombre.lower():
+            return self.nombre  # Evita redundancia
+        return f"{self.nombre} ({self.categoria.nombre} - {self.categoria.grupo.nombre})"
+
 class Prenda(models.Model):
-    id = models.AutoField(primary_key=True) 
+    id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
     talla = models.CharField(max_length=10)
     precio = models.DecimalField(max_digits=8, decimal_places=2)
-    estado = models.CharField(max_length=50) #Ej: "nueva", "En buen estado", etc...
+    estado = models.CharField(max_length=50)
     disponible = models.BooleanField(default=True)
     imagen = models.ImageField(upload_to='prendas/')
     fecha_agregado = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.nombre
-    
-class Categoria(models.Model):
-    nombre = models.CharField(max_length=100)
+    # NUEVA RELACIÓN
+    subcategoria = models.ForeignKey(Subcategoria, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.nombre
