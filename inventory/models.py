5e08822a2d48a14e08b338fd66f96b8c49f1399c
JuanJose <jjrodrigur@eafit.edu.co>
2025-05-26 19:02:19 -0500

    Actualización del modelo de prendas: Codigo de identificacion interno

--- inventory/models.py
+++ inventory/models.py
@@ -26,6 +26,7 @@ class Subcategoria(models.Model):
         return f"{self.nombre} ({self.categoria.nombre} - {self.categoria.grupo.nombre})"
 
 class Prenda(models.Model):
+    codigo = models.CharField(max_length=20, unique=True)  # nuevo campo para ID personalizada
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
@@ -40,4 +41,4 @@ class Prenda(models.Model):
     subcategoria = models.ForeignKey(Subcategoria, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
-        return self.nombre
+        return self.codigo
